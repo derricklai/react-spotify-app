cc632b02e246b395c5854023905bd1d0bb142a19
derricklai <derrickrlai@gmail.com>
2024-07-27 14:11:43 +1000

    feat: update page to render spotify api

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,50 @@
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+import { accessToken, getCurrentUserProfile, logout } from "./spotify";
+import { catchErrors } from "./utils";
 import "./App.css";
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    setToken(accessToken);
+
+    const fetchData = async () => {
+      const { data } = await getCurrentUserProfile();
+
+      console.log(profile); // delete this later
+
+      setProfile(data);
+    };
+
+    catchErrors(fetchData());
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="http://localhost:8888/login"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Log in to Spotify
-        </a>
+        {!token ? (
+          <a className="App-link" href="http://localhost:8888/login">
+            Log in to Spotify
+          </a>
+        ) : (
+          <>
+            <h1>Logged in!</h1>
+            <button onClick={logout}>Log Out</button>
+
+            {profile && (
+              <div>
+                <h1>{profile.display_name}</h1>
+                <p>{profile.followers.total} Followers</p>
+                {profile.images.length && profile.images[1].url && (
+                  <img src={profile.images[1].url} alt="Avatar" />
+                )}
+              </div>
+            )}
+          </>
+        )}
       </header>
     </div>
   );
