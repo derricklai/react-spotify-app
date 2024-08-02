3c7e964934c12f850383603fa7225c747be35fde
derricklai <derrickrlai@gmail.com>
2024-08-02 20:17:02 +1000

    feat: add Login Profile pages + styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import { accessToken, getCurrentUserProfile, logout } from "./spotify";
 import { catchErrors } from "./utils";
-import styled from "styled-components/macro";
 import { GlobalStyle } from "./styles";
+import { Login, Profile } from "./pages";
 
 // import {
 //   BrowserRouter as Router,
@@ -10,15 +10,6 @@ import { GlobalStyle } from "./styles";
 //   Route,
 //   ScrollRestoration,
 // } from "react-router-dom";
-
-const StyledLoginButton = styled.a`
-  background-color: green;
-  color: white;
-  padding: 10px 20px;
-  margin: 20px auto;
-  border-radius: 30px;
-  display: inline-block;
-`;
 
 function App() {
   const [token, setToken] = useState(null);
@@ -44,44 +35,19 @@ function App() {
     <div className="App">
       <GlobalStyle />
       <header className="App-header">
-        {/* {!token ? (
-          
-        ) : null} */}
-        <StyledLoginButton
-          className="App-link"
-          href="http://localhost:8888/login"
-        >
-          Log in to Spotify
-        </StyledLoginButton>
+        {!token ? (
+          <Login />
+        ) : // <Router>
+        //   <ScrollRestoration />
 
-        {/* <Router>
-            <ScrollRestoration />
-
-            <Routes>
-              <Route path="/top-artists" element={<h1>Top Artists</h1>} />
-              <Route path="/top-tracks" element={<h1>Top Tracks</h1>} />
-              <Route path="/playlists/:id" element={<h1>Playlist</h1>} />
-              <Route path="/playlists" element={<h1>Playlists</h1>} />
-              <Route
-                path="/"
-                element={
-                  <>
-                    <button onClick={logout}>Log Out</button>
-
-                    {profile && (
-                      <div>
-                        <h1>{profile.display_name}</h1>
-                        <p>{profile.followers.total} Followers</p>
-                        {profile.images.length && profile.images[0].url && (
-                          <img src={profile.images[0].url} alt="Avatar" />
-                        )}
-                      </div>
-                    )}
-                  </>
-                }
-              />
-            </Routes>
-          </Router> */}
+        //   <Routes>
+        //     <Route path="/">
+        //       <Profile />
+        //     </Route>
+        //   </Routes>
+        // </Router>
+        // )}
+        null}
       </header>
     </div>
   );
