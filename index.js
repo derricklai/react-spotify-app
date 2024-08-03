63430c58f1e27ba73b6203462efd6d0ca174ff85
derricklai <derrickrlai@gmail.com>
2024-08-03 17:25:24 +1000

    feat: add scope

--- index.js
+++ index.js
@@ -31,13 +31,15 @@ const generateRandomString = (length) => {
   return text;
 };
 
-const stateKey = "spoitfy_auth_state";
+const stateKey = "spotify_auth_state";
 
 app.get("/login", (req, res) => {
   const state = generateRandomString(16);
   res.cookie(stateKey, state);
 
-  const scope = "user-read-private user-read-email";
+  const scope = ["user-read-private", "user-read-email", "user-top-read"].join(
+    " "
+  );
 
   const queryParams = querystring.stringify({
     client_id: CLIENT_ID,
@@ -46,6 +48,7 @@ app.get("/login", (req, res) => {
     state: state,
     scope: scope,
   });
+
   res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
