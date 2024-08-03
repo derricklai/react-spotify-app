1960b6face47d31f50afb2d2706990a168756643
derricklai <derrickrlai@gmail.com>
2024-08-04 02:24:30 +1000

    add vercel test

--- server/index.js
+++ server/index.js
@@ -84,7 +84,7 @@ app.get("/callback", (req, res) => {
           refresh_token,
         });
 
-        res.redirect(`https://${FRONTEND_URI}/?${queryParams}`);
+        res.redirect(`${FRONTEND_URI}/?${queryParams}`);
       } else {
         res.redirect(`/?${querystring.stringify({ error: "invalid_token" })}`);
       }
